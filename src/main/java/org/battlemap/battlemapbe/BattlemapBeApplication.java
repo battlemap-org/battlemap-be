480c75435487844f26bd8dd5e62154a91a14545c
Heejin <heejin0283@example.com>
2025-10-25 09:50:53 +0900

    fix: BattlemapBeApplication 실행 로직 수정

--- src/main/java/org/battlemap/battlemapbe/BattlemapBeApplication.java
+++ src/main/java/org/battlemap/battlemapbe/BattlemapBeApplication.java
@@ -2,10 +2,11 @@ package org.battlemap.battlemapbe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BattlemapBeApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(BattlemapBeApplication.class, args);
     }
