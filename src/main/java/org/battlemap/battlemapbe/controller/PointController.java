8f69b0b2cc3c39efead3f7a9ff03e881f7ae3900
김민지 <meeji12345@naver.com>
2025-11-04 12:09:38 +0900

    포인트 조회 api 수정

--- src/main/java/org/battlemap/battlemapbe/controller/PointController.java
+++ src/main/java/org/battlemap/battlemapbe/controller/PointController.java
@@ -5,6 +5,7 @@ import org.battlemap.battlemapbe.model.response.ApiResponse;
 import org.battlemap.battlemapbe.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,8 +19,9 @@ public class PointController {
     private final UserService userService;
 
     // 사용자 포인트 조회 api
-    @GetMapping("/points")
-    public ResponseEntity<ApiResponse<Map<String, Integer>>> getUserPoints(@AuthenticationPrincipal String loginId) {
+    @GetMapping
+    public ResponseEntity<ApiResponse<Map<String, Integer>>> getUserPoints(Authentication authentication) {
+        String loginId = authentication.getName();
         int currentPoint = userService.getUserPoints(loginId);
         return ResponseEntity.ok(
                 ApiResponse.success(Map.of("currentPoint", currentPoint), 200)
